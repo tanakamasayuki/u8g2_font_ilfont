4847bf7a2c262921fa01795e03a51339bafe3088
root <akiraneko@gmail.com>
2020-11-30 17:33:19 +0900

    Fix efont

--- lgfx_fonts/lgfx_efont_cn.h
+++ lgfx_fonts/lgfx_efont_cn.h
@@ -17,6 +17,10 @@ extern const uint8_t lgfx_efont_cn_16[];
 extern const uint8_t lgfx_efont_cn_16_b[];
 extern const uint8_t lgfx_efont_cn_16_bi[];
 extern const uint8_t lgfx_efont_cn_16_i[];
+extern const uint8_t lgfx_efont_cn_24[];
+extern const uint8_t lgfx_efont_cn_24_b[];
+extern const uint8_t lgfx_efont_cn_24_bi[];
+extern const uint8_t lgfx_efont_cn_24_i[];
 
 static const lgfx::U8g2font efont_cn_10(lgfx_efont_cn_10);
 static const lgfx::U8g2font efont_cn_10_b(lgfx_efont_cn_10_b);
@@ -34,3 +38,7 @@ static const lgfx::U8g2font efont_cn_16(lgfx_efont_cn_16);
 static const lgfx::U8g2font efont_cn_16_b(lgfx_efont_cn_16_b);
 static const lgfx::U8g2font efont_cn_16_bi(lgfx_efont_cn_16_bi);
 static const lgfx::U8g2font efont_cn_16_i(lgfx_efont_cn_16_i);
+static const lgfx::U8g2font efont_cn_24(lgfx_efont_cn_24);
+static const lgfx::U8g2font efont_cn_24_b(lgfx_efont_cn_24_b);
+static const lgfx::U8g2font efont_cn_24_bi(lgfx_efont_cn_24_bi);
+static const lgfx::U8g2font efont_cn_24_i(lgfx_efont_cn_24_i);
